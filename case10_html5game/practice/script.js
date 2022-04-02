1d993c36dfec05192fc0c4507ec7d2ee63b57c0d
literate <gaekoon@gmail.com>
2022-04-02 23:21:23 +0900

    completed worms

--- case10_html5game/practice/script.js
+++ case10_html5game/practice/script.js
@@ -18,9 +18,9 @@
     food: "rgb(66, 187, 103)",
   };
 
-  let start = null;
-  let option = {
-    highscore: localStorage.getItem("score") || 0,
+  let start = 0;
+  let setup = {
+    highScore: localStorage.getItem("score") || 0,
     gameEnd: true,
     direction: 2,
     snake: [
@@ -34,22 +34,23 @@
   };
 
   const init = () => {
-    // document.addEventListener("keydown", (e) => {
-    //   // 화살표 키가 아니면 수행하지 않도록
-    //   if (!/Arrow/gi.test(e.key)) {
-    //     return;
-    //   }
-    //   e.preventDefault();
-    //   const direction = getDirection(e.key);
-    //   if (!isDirectionCorrect(direction)) {
-    //     return;
-    //   }
-    //   option.direction = direction;
-    // });
+    document.addEventListener("keydown", (e) => {
+      // 화살표 키가 아니면 수행하지 않도록
+      if (!/Arrow/gi.test(e.key)) {
+        return;
+      }
+      e.preventDefault();
+      const direction = getDirection(e.key);
+      if (!isDirectionCorrect(direction)) {
+        return;
+      }
+      setup.direction = direction;
+    });
+
     $play.onclick = () => {
-      if (option.gameEnd) {
-        option = {
-          highscore: localStorage.getItem("score") || 0,
+      if (setup.gameEnd) {
+        setup = {
+          highScore: localStorage.getItem("score") || 0,
           gameEnd: false,
           direction: 2,
           snake: [
@@ -58,11 +59,12 @@
             { x: 10, y: 30, direction: 2 },
           ],
           food: { x: 0, y: 0 },
+          score: 0,
         };
-        $score.innerText = `점수: 0점`;
-        $highscore.innerText = `최고점수: ${option.highscore}점`;
+        $score.textContent = `점수: ${setup.score}점`;
+        $highscore.textContent = `최고점수: ${setup.highScore}점`;
 
-        // randomFood();
+        randomFood();
         window.requestAnimationFrame(play);
       }
     };
@@ -86,31 +88,177 @@
   };
 
   const setSnake = () => {
-    for (let i = option.snake.length - 1; i >= 0; --i) {
-      buildSnake(ctx, option.snake[i].x, option.snake[i].y, i === 0);
+    // for (let i = 0; i < setup.snake.length; ++i) {
+    //   buildSnake(ctx, setup.snake[i].x, setup.snake[i].y, i === 0);
+    // }
+    for (let i = setup.snake.length - 1; i >= 0; --i) {
+      buildSnake(ctx, setup.snake[i].x, setup.snake[i].y, i === 0);
+    }
+  };
+
+  const setDirection = (number, value) => {
+    while (value < 0) {
+      value += number;
+    }
+
+    return value % number;
+  };
+
+  const setBody = () => {
+    const snake = setup.snake;
+    const tail = snake[snake.length - 1];
+    const direction = snake.direction;
+    let x = tail.x;
+    let y = tail.y;
+
+    switch (direction) {
+      // down
+      case 1:
+        y = setDirection(300, y - 10);
+        break;
+      // up
+      case -1:
+        y = setDirection(300, y + 10);
+        break;
+      // left
+      case -2:
+        x = setDirection(300, x + 10);
+        break;
+      // right
+      case 2:
+        x = setDirection(300, x - 10);
+        break;
+    }
+    setup.snake.push({ x, y, direction });
+  };
+
+  const getFood = () => {
+    const snakeX = setup.snake[0].x;
+    const snakeY = setup.snake[0].y;
+    const foodX = setup.food.x;
+    const foodY = setup.food.y;
+
+    if (snakeX === foodX && snakeY === foodY) {
+      setup.score++;
+      $score.textContent = `점수 : ${setup.score}점`;
+      setBody(); // 지렁이 성장
+      randomFood();
+    }
+  };
+
+  const randomFood = () => {
+    let x = Math.floor(Math.random() * 25) * 10;
+    let y = Math.floor(Math.random() * 25) * 10;
+    while (setup.snake.some((part) => part.x === x && part.y === y)) {
+      x = Math.floor(Math.random() * 25) * 10;
+      y = Math.floor(Math.random() * 25) * 10;
+    }
+    setup.food = { x, y };
+  };
+
+  const moveSnake = () => {
+    let x = setup.snake[0].x;
+    let y = setup.snake[0].y;
+
+    switch (setup.direction) {
+      // down
+      case 1:
+        y = setDirection(300, y + 10);
+        break;
+      // up
+      case -1:
+        y = setDirection(300, y - 10);
+        break;
+      // left
+      case -2:
+        x = setDirection(300, x - 10);
+        break;
+      // right
+      case 2:
+        x = setDirection(300, x + 10);
+        break;
+    }
+
+    const snake = [{ x, y, direction: setup.direction }];
+    const snakeLength = setup.snake.length;
+
+    // 대가리 위치만 새로 만들고
+    // 원래 지렁이의 snake[0], snake[1]만 그린다
+    // 어차피 setSnake할 때 snake[0]을 head로 인식해서 색을 칠해줌
+    for (let index = 0; index < snakeLength - 1; index++) {
+      snake.push({ ...setup.snake[index] });
+    }
+    setup.snake = snake;
+  };
+
+  const getDirection = (key) => {
+    let direction = 0;
+    switch (key) {
+      case "ArrowDown":
+        direction = 1;
+        break;
+      case "ArrowUp":
+        direction = -1;
+        break;
+      case "ArrowLeft":
+        direction = -2;
+        break;
+      case "ArrowRight":
+        direction = 2;
+        break;
+    }
+    return direction;
+  };
+
+  const isDirectionCorrect = (direction) => {
+    return (
+      setup.direction === setup.snake[0].direction &&
+      setup.direction !== -direction
+    );
+  };
+
+  const isGameOver = () => {
+    const snake = setup.snake;
+    const head = snake[0];
+    // return snake.some(
+    //   (el) => head !== el && el.x === head.x && el.y === head.y
+    // );
+    return snake.some(
+      (el, index) => index !== 0 && el.x === head.x && el.y === head.y
+    );
+  };
+
+  const setHighScore = () => {
+    const localScore = setup.highScore * 1 || 0;
+    const finalScore = $score.textContent.match(/\d+/)[0] * 1;
+    if (localScore < finalScore) {
+      alert(`최고기록: ${finalScore}점`);
+      localStorage.setItem("score", finalScore);
     }
   };
 
   const play = (timestamp) => {
-    if (option.gameEnd) {
+    ++start;
+    if (setup.gameEnd) {
       return;
     }
 
-    if (null == start) {
-      start = timestamp;
+    if (timestamp - start > 1000 / 10) {
+      if (isGameOver()) {
+        setup.gameEnd = true;
+        setHighScore();
+        alert("Game Over");
+        return;
+      }
+      moveSnake();
       buildBoard();
-      buildFood(ctx, option.food.x, option.food.y);
+      buildFood(ctx, setup.food.x, setup.food.y);
       setSnake();
+      getFood();
+      start = timestamp;
     }
 
     window.requestAnimationFrame(play);
-
-    // if (isGameOver()) {
-    //   option.gameEnd = true;
-    //   setHighScore();
-    //   alert("Game Over");
-    //   return;
-    // }
   };
 
   init();
